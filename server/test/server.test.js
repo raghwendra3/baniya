5e799cb4192aed3d06ac832c45f52db0895b2353
raghwendra3 <raghudada93@gmail.com>
2018-01-17 22:47:52 -0800

    Add test for get/todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,8 +4,16 @@ const mocha = require('mocha');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 console.log("beforeeach");
+
+const todos =[{
+  text: 'First test todo'
+},{
+  text: 'Second test todo'
+}];
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+  return Todo.insertMany(todos);
+}).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -42,9 +50,21 @@ describe('POST /todos', () => {
         return done(err);
       }
       Todo.find().then((todos) => {
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(2);
         done();
       }).catch((e) => done(e));
     });
   });
+});
+
+describe('/GET / todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todos.length).toBe(2);
+    })
+    .end(done);
+});
 });
